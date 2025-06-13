51741a47e34066bb7d2439dd76ce51d25c863eb7
Vidhi <vidhij0815@gmail.com>
2025-06-13 11:56:59 +0530

    vidhi

---
+++ UserDiscount.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class UserDiscount {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fee, discountPercent;
+        System.out.print("Enter fee: ");
+        fee = input.nextDouble();
+        System.out.print("Enter discount percent: ");
+        discountPercent = input.nextDouble();
+        double discount = (discountPercent / 100) * fee;
+        double finalFee = fee - discount;
+        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + finalFee);
+    }
+}
