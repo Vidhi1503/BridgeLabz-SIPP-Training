3d12652ecc31b40470d3daa8e2a87c5095449daf
Vidhi <vidhij0815@gmail.com>
2025-06-17 22:35:02 +0530

    vidhi

---
+++ Day2_Programming Elements/sum0.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Q7 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    double sum = 0, n;
+    while ((n = s.nextDouble()) != 0) sum += n;
+    System.out.println("Sum = " + sum);
+  }
+}
