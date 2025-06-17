d87731a34666f8171a0c94fa9861a79bdcec8966
Vidhi <vidhij0815@gmail.com>
2025-06-17 22:50:54 +0530

    vidhi

---
+++ Day2_Programming Elements/Q12.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Q12 {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int n = sc.nextInt();
+    System.out.println(n > 0 ? "Positive" : n < 0 ? "Negative" : "Zero");
+  }
+}
