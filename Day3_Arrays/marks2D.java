79cc5916d85242a25094e687d647bcf8ea065d01
Vidhi <vidhij0815@gmail.com>
2025-06-18 11:52:35 +0530

    vidhi

---
+++ Day3_Arrays/marks2D.java
@@ -0,0 +1,22 @@
+package Day3_Arrays;
+
+import java.util.*;
+class marks2D {
+  public static void main(String[] a) {
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    int[][] m = new int[n][3];
+    double[] pct = new double[n];
+    char[] grade = new char[n];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < 3; j++) {
+        m[i][j] = s.nextInt();
+        if (m[i][j] < 0) { j--; continue; }
+      }
+      pct[i] = (m[i][0] + m[i][1] + m[i][2]) / 3.0;
+      grade[i] = pct[i] >= 90 ? 'A' : (pct[i] >= 75 ? 'B' : (pct[i] >= 60 ? 'C' : 'D'));
+    }
+    for (int i = 0; i < n; i++)
+      System.out.println(Arrays.toString(m[i]) + " " + pct[i] + " " + grade[i]);
+  }
+}
