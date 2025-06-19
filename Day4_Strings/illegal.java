bbd9bfa8071aa34b5da64324d8184bb459861240
Vidhi <vidhij0815@gmail.com>
2025-06-19 11:52:18 +0530

    vidhi

---
+++ Day4_Strings/illegal.java
@@ -0,0 +1,8 @@
+package Day4_Strings;
+
+public class illegal {
+    public static void main(String[] args) {
+        String text = "hello";
+        System.out.println(text.substring(3, 2));
+    }
+}
