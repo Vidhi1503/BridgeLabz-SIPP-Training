234857ccf3991135876cfff9c2f082ffc048292d
Vidhi <vidhij0815@gmail.com>
2025-06-19 10:41:22 +0530

    vidhi

---
+++ Day4_Strings/array_index.java
@@ -0,0 +1,8 @@
+package Day4_Strings;
+
+public class array_index {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob"};
+        System.out.println(names[5]);
+    }
+}
