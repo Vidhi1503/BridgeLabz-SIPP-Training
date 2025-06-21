d2e1808c36957f2282e36e18655e2f3a9ede841c
Vidhi <vidhij0815@gmail.com>
2025-06-21 21:10:06 +0530

    vidhi

---
+++ Day6_EXTRAS_STRING/replace.java
@@ -0,0 +1,8 @@
+package Day6_EXTRAS_STRING;
+
+public class replace {
+  public static void main(String[] a) {
+    String str1 = "Java is fun", str2 = "fun", str3 = "awesome";
+    System.out.println(str1.replace(str2,str3));
+  }
+}
