393bf1738e263ed963727ce22a74ee203ae95fce
Vidhi Khandelwal <122196946+Vidhi1503@users.noreply.github.com>
2025-06-23 12:19:07 +0530

    Delete Day2_Control Flows directory

--- Day2_Control Flows/fact2.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-class fact2 {
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int n = sc.nextInt(), f = 1, i = 1;
-    if (n >= 0) while (i <= n) f *= i++;
-    System.out.println("Factorial = " + f);
-  }
-}
