104b9ede1b69abe1c1a6b6e77e9f44701b027e57
Vidhi Khandelwal <122196946+Vidhi1503@users.noreply.github.com>
2025-06-23 12:19:51 +0530

    Delete Day4_Strings directory

--- Day4_Strings/illegal.java
+++
@@ -1,8 +0,0 @@
-package Day4_Strings;
-
-public class illegal {
-    public static void main(String[] args) {
-        String text = "hello";
-        System.out.println(text.substring(3, 2));
-    }
-}
