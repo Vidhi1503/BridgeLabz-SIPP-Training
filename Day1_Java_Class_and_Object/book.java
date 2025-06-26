51062f29f870158490a400c56b50a240727a2831
Vidhi <vidhij0815@gmail.com>
2025-06-26 20:29:00 +0530

    vidhi

---
+++ Day1_Java_Class_and_Object/book.java
@@ -0,0 +1,10 @@
+package Day1_Java_Class_and_Object;
+
+class book {
+    String title, author;
+    double price;
+
+    void display() {
+        System.out.println(title + " " + author + " " + price);
+    }
+}
--- Day1_Java_Class_and_Object/circle.java
+++ Day1_Java_Class_and_Object/circle.java
@@ -1,6 +1,6 @@
 package Day1_Java_Class_and_Object;
 
-public class c {
+public class circle {
     double radius;
     double area() { return Math.PI * radius * radius; }
     double circumference() { return 2 * Math.PI * radius; }
