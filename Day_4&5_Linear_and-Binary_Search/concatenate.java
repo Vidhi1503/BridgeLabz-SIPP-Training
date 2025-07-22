e94692d27a8a9986c3e4401ba9f95610ed7b8f5e
Vidhi Khandelwal <122196946+Vidhi1503@users.noreply.github.com>
2025-07-22 20:36:27 +0530

    Delete Day_4&5_Linear_and-Binary_Search directory

--- Day_4&5_Linear_and-Binary_Search/concatenate.java
+++
@@ -1,9 +0,0 @@
-public class concatenate {
-    public static void main(String[] args) {
-        String[] arr = {"Java", "is", "fun"};
-        StringBuffer sb = new StringBuffer();
-        for (String s : arr) sb.append(s);
-        System.out.println(sb);
-    }
-}
-
--- Day_4&5_Linear_and-Binary_Search/reverse.java
+++
@@ -1,7 +0,0 @@
-public class reverse {
-    public static void main(String[] args) {
-        String s = "hello";
-        String reversed = new StringBuilder(s).reverse().toString();
-        System.out.println(reversed);
-    }
-}
