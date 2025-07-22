4f83f5189aa285333b38af55a983ca4e4f9bf076
Vidhi <vidhij0815@gmail.com>
2025-07-22 21:02:05 +0530

    vidhi

---
+++ Day1_Java_Class_and_Object/employee.java
@@ -0,0 +1,11 @@
+package Day1_Java_Class_and_Object;
+
+public class employee {
+    String name;
+    int id;
+    double salary;
+
+    void display() {
+        System.out.println(name + " " + id + " " + salary);
+    }
+}
