53407520ae1c74416381b835d04a16d60d877be7
Vidhi <vidhij0815@gmail.com>
2025-08-28 11:14:31 +0530

    vidhi

---
+++ Day3_Java_Streams/UserInput.java
@@ -0,0 +1,18 @@
+package Day3_Java_Streams;
+
+import java.io.*;
+class UserInput {
+    public static void main(String[] a)throws Exception {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Name: "); 
+        String n=br.readLine();
+        System.out.print("Age: "); 
+        String age=br.readLine();
+        System.out.print("Lang: "); 
+        String lang=br.readLine();
+        try(FileWriter fw=new FileWriter("user.txt")){
+            fw.write(n+" "+age+" "+lang);
+        }
+        System.out.println("Saved.");
+    }
+}
