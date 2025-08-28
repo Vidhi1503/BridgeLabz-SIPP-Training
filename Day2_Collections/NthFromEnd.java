eb3832f3d1458a0ded0c734f69624d4692b805cc
Vidhi <vidhij0815@gmail.com>
2025-08-28 11:01:01 +0530

    vidhi

---
+++ Day2_Collections/NthFromEnd.java
@@ -0,0 +1,13 @@
+package Day2_Collections;
+
+import java.util.*;
+class NthFromEnd {
+    public static void main(String[] args) {
+        LinkedList<String> list=new LinkedList<>(Arrays.asList("A","B","C","D","E"));
+        int n=2;
+        var p1=list.listIterator(),p2=list.listIterator();
+        for(int i=0;i<n;i++) p2.next();
+        while(p2.hasNext()){p1.next();p2.next();}
+        System.out.println(p1.next());
+    }
+}
