c91f2e77f75f6a5f6bb202ff5e2ef86c363b2f0d
Vidhi Khandelwal <122196946+Vidhi1503@users.noreply.github.com>
2025-09-10 12:05:22 +0530

    Delete Day5_Regex_&_JUnit directory

--- Day5_Regex_&_JUnit/Basic Regex Problems/ValidateLicense.java
+++
@@ -1,6 +0,0 @@
-public class ValidateLicense {
-    public static void main(String[] args) {
-        String plate = "AB1234";
-        System.out.println(plate.matches("^[A-Z]{2}\\d{4}$") ? "Valid" : "Invalid");
-    }
-}
