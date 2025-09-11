c87a0c4a997aa922502aa7e075af975ba5d41f0f
Vidhi <vidhij0815@gmail.com>
2025-09-11 10:29:52 +0530

    vidhi

---
+++ Day1_Lambda_Expressions/a.java
@@ -0,0 +1,13 @@
+package Day1_Lambda_Expressions;
+
+import java.util.*;
+import java.util.function.Predicate;
+
+public class HospitalAlerts {
+    public static void main(String[] args) {
+        List<String> alerts = Arrays.asList("Critical: Heart", "Info: BP Check", "Critical: Oxygen");
+
+        Predicate<String> onlyCritical = a -> a.startsWith("Critical");
+        alerts.stream().filter(onlyCritical).forEach(System.out::println);
+    }
+}
