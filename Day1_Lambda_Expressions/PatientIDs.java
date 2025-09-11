7720dc72441963f4187b23ca91e850b289edd24e
Vidhi <vidhij0815@gmail.com>
2025-09-11 11:02:56 +0530

    vidhi

---
+++ Day1_Lambda_Expressions/PatientIDs.java
@@ -0,0 +1,10 @@
+package Day1_Lambda_Expressions;
+
+import java.util.*;
+
+public class PatientIDs {
+    public static void main(String[] args) {
+        List<Integer> ids = Arrays.asList(101,102,103,104);
+        ids.forEach(System.out::println); // method reference
+    }
+}
