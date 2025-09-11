96e0b7e9f56078ecf21b01b94511c2accf6e9f75
Vidhi <vidhij0815@gmail.com>
2025-09-11 11:40:35 +0530

    vidhi

---
+++ Day3_Stream_API/AttendeeWelcome.java
@@ -0,0 +1,9 @@
+package Day3_Stream_API;
+
+import java.util.*;
+public class AttendeeWelcome{
+  public static void main(String[]a){
+    List<String>att=Arrays.asList("Vidhi","Kiran","Amit");
+    att.forEach(x->System.out.println("Welcome "+x+"!"));
+  }
+}
